946c6d4a106baaf7d7135c8b192a19859fbcd70f
Alan Palazzolo <github@zzolo.org>
2011-10-25 00:05:04 -0700

    Adding events.

--- js/tile-compare.js
+++ js/tile-compare.js
@@ -14,57 +14,67 @@
     var headerHeight = $('header').height();
     var windowHeight = $(window).height() - headerHeight;
     var windowWidth = $(window).width();
+    var maps = [];
     
     // Google
-    var mapGoogle = new OpenLayers.Map();
+    maps['mapGoogle'] = new OpenLayers.Map();
     var layer = new OpenLayers.Layer.Google('Google Streets');
-    mapGoogle.addLayer(layer);
+    maps['mapGoogle'].addLayer(layer);
     var panelGoogle = new GeoExt.MapPanel({
       renderTo: 'map1',
       height: Math.floor(windowHeight / 2),
       width: Math.floor(windowWidth / 2),
-      map: mapGoogle,
+      map: maps['mapGoogle'],
       title: 'Google Street'
     });
     
     // Bing
-    var mapBing = new OpenLayers.Map();
+    maps['mapBing'] = new OpenLayers.Map();
     var apiKey = "AqTGBsziZHIJYYxgivLBf0hVdrAk9mWO5cQcb8Yux8sW5M8c8opEC2lZqKR1ZZXf";
     var road = new OpenLayers.Layer.Bing({ key: apiKey, type: 'Road' });
     var aerial = new OpenLayers.Layer.Bing({ key: apiKey, type: 'Aerial' });
-    mapBing.addLayer(road);
+    maps['mapBing'].addLayer(road);
     var panelBing = new GeoExt.MapPanel({
       renderTo: 'map2',
       height: Math.floor(windowHeight / 2),
       width: Math.floor(windowWidth / 2),
-      map: mapBing,
+      map: maps['mapBing'],
       title: 'Bing Aerial'
     });
     
     // OSM
-    var mapOSM = new OpenLayers.Map();
+    maps['mapOSM'] = new OpenLayers.Map();
     var layer = new OpenLayers.Layer.OSM( "Simple OSM Map");
-    mapOSM.addLayer(layer);
+    maps['mapOSM'].addLayer(layer);
     var panelOSM = new GeoExt.MapPanel({
       renderTo: 'map3',
       height: Math.floor(windowHeight / 2),
       width: Math.floor(windowWidth / 2),
-      map: mapOSM,
+      map: maps['mapOSM'],
       title: 'OSM'
     });
     
     // Yahoo
-    var mapYahoo = new OpenLayers.Map();
+    maps['mapYahoo'] = new OpenLayers.Map();
     var layer = new OpenLayers.Layer.Yahoo( "Yahoo");
-    mapYahoo.addLayer(layer);
+    maps['mapYahoo'].addLayer(layer);
     var panelYahoo = new GeoExt.MapPanel({
       renderTo: 'map4',
       height: Math.floor(windowHeight / 2),
       width: Math.floor(windowWidth / 2),
-      map: mapYahoo,
+      map: maps['mapYahoo'],
       title: 'Yahoo'
     });
     
-  });
+    
+    // Event listeners
+    for (var i in maps) {
+      if (maps.hasOwnProperty(i)) {
+        maps[i].events.on('moveend', function(e) { console.log(e); });
+        maps[i].events.on('zoomend', function(e) { console.log(e); });
+      }
+    }
 
+    
+  });
 })(jQuery);
