8ad5c94f158b95b3fd3e47eb90919b23eb00562e
Alan Palazzolo <github@zzolo.org>
2011-10-25 14:10:40 -0700

    Made it so that maps were defined in object, less code.

--- js/tile-compare.js
+++ js/tile-compare.js
@@ -14,67 +14,66 @@
     var headerHeight = $('header').height();
     var windowHeight = $(window).height() - headerHeight;
     var windowWidth = $(window).width();
-    var maps = [];
+    var bingKey = 'AqTGBsziZHIJYYxgivLBf0hVdrAk9mWO5cQcb8Yux8sW5M8c8opEC2lZqKR1ZZXf';
+    var maps = {};
     
-    // Google
-    maps['mapGoogle'] = new OpenLayers.Map();
-    var layer = new OpenLayers.Layer.Google('Google Streets');
-    maps['mapGoogle'].addLayer(layer);
-    var panelGoogle = new GeoExt.MapPanel({
-      renderTo: 'map1',
-      height: Math.floor(windowHeight / 2),
-      width: Math.floor(windowWidth / 2),
-      map: maps['mapGoogle'],
-      title: 'Google Street'
-    });
+    // Data
+    maps = {
+      'google': {
+        'title': 'Google',
+        'layers': [ new OpenLayers.Layer.Google('Google Streets') ]
+      },
+      'bing': {
+        'title': 'Bing',
+        'layers': [ 
+          new OpenLayers.Layer.Bing({ name: 'Bing Streets', key: bingKey, type: 'Road' }), 
+          new OpenLayers.Layer.Bing({ name: 'Bing Aerial', key: bingKey, type: 'Aerial' }) ]
+      },
+      'yahoo': {
+        'title': 'Yahoo',
+        'layers': [ new OpenLayers.Layer.Yahoo('Yahoo Streets') ]
+      },
+      'osm': {
+        'title': 'OSM',
+        'layers': [ new OpenLayers.Layer.OSM('OSM Default') ]
+      }
+    };
     
-    // Bing
-    maps['mapBing'] = new OpenLayers.Map();
-    var apiKey = "AqTGBsziZHIJYYxgivLBf0hVdrAk9mWO5cQcb8Yux8sW5M8c8opEC2lZqKR1ZZXf";
-    var road = new OpenLayers.Layer.Bing({ key: apiKey, type: 'Road' });
-    var aerial = new OpenLayers.Layer.Bing({ key: apiKey, type: 'Aerial' });
-    maps['mapBing'].addLayer(road);
-    var panelBing = new GeoExt.MapPanel({
-      renderTo: 'map2',
-      height: Math.floor(windowHeight / 2),
-      width: Math.floor(windowWidth / 2),
-      map: maps['mapBing'],
-      title: 'Bing Aerial'
-    });
-    
-    // OSM
-    maps['mapOSM'] = new OpenLayers.Map();
-    var layer = new OpenLayers.Layer.OSM( "Simple OSM Map");
-    maps['mapOSM'].addLayer(layer);
-    var panelOSM = new GeoExt.MapPanel({
-      renderTo: 'map3',
-      height: Math.floor(windowHeight / 2),
-      width: Math.floor(windowWidth / 2),
-      map: maps['mapOSM'],
-      title: 'OSM'
-    });
-    
-    // Yahoo
-    maps['mapYahoo'] = new OpenLayers.Map();
-    var layer = new OpenLayers.Layer.Yahoo( "Yahoo");
-    maps['mapYahoo'].addLayer(layer);
-    var panelYahoo = new GeoExt.MapPanel({
-      renderTo: 'map4',
-      height: Math.floor(windowHeight / 2),
-      width: Math.floor(windowWidth / 2),
-      map: maps['mapYahoo'],
-      title: 'Yahoo'
-    });
-    
-    
-    // Event listeners
+    // Got through maps
+    var count = 0;
     for (var i in maps) {
       if (maps.hasOwnProperty(i)) {
-        maps[i].events.on('moveend', function(e) { console.log(e); });
-        maps[i].events.on('zoomend', function(e) { console.log(e); });
+        count++;
+        
+        // Define controls
+        var controls = [
+          new OpenLayers.Control.LayerSwitcher({'ascending':false}),
+          //new OpenLayers.Control.Permalink('permalink'),
+          new OpenLayers.Control.KeyboardDefaults(),
+          new OpenLayers.Control.Attribution()
+        ];
+        
+        // Create new map and controls
+        maps[i].map = new OpenLayers.Map('', { controls: controls });
+        
+        // Add layers
+        maps[i].map.addLayers(maps[i].layers);
+        
+        // Create panel
+        maps[i].panel = new GeoExt.MapPanel({
+          renderTo: 'map' + count,
+          height: Math.floor(windowHeight / 2),
+          width: Math.floor(windowWidth / 2),
+          map: maps[i].map,
+          title: maps[i].title
+        });
+        
+        // Set center and zoom
+        maps[i].map.setCenter(new OpenLayers.LonLat(100,10), 4);
+        
+        //maps[i].events.on('moveend', function(e) { console.log(e); });
+        //maps[i].events.on('zoomend', function(e) { console.log(e); });
       }
     }
-
-    
   });
 })(jQuery);
